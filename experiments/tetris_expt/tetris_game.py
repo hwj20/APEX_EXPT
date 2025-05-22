819acbd471d56bc0d6dd6416350b832ff52ee880
Wanjing Huang <42178907+hwj20@users.noreply.github.com>
2025-05-22 14:20:05 -0700

    Update tetris_game.py

--- experiments/tetris_expt/tetris_game.py
+++ experiments/tetris_expt/tetris_game.py
@@ -164,19 +164,19 @@ if __name__ == "__main__":
     # run_tetris("APEX", rng=random.Random(42),auto_gen_path=True)
     # run_tetris("gpt-4o", rng=random.Random(42),auto_gen_path=True)
     # run_tetris("VLM_APEX", rng=random.Random(42),auto_gen_path=True)
-    run_tetris("CLIP", rng=random.Random(42),auto_gen_path=True)
+    # run_tetris("CLIP", rng=random.Random(42),auto_gen_path=True)
     # run_tetris("VLM", rng=random.Random(42),auto_gen_path=True)
     # run_tetris("gpt-4o-mini", rng=random.Random(42),auto_gen_path=True)
     # run_tetris("o3-mini", rng=random.Random(42),auto_gen_path=True)
-    # for model in results.keys():
-    #     for i in range(5):  # 5 trails
-    #         save_path = f"results/tetris_results/{model}_{i}_tetris_game_history.json"
-    #         results = run_tetris(model, save_path, rng=random.Random(42 + i))
-    #
-    #         results[model]["final_score"].append(results["final_score"])
-    #         results[model]["max_stack_height"].append(results["max_stack_height"])
-    #         results[model]["holes"].append(results["holes"])
-    #         results[model]["bumps"].append(results["bumps"])
-    #         results[model]["height_delta_per_move"].append(results["height_delta_per_move"])
-    #         with open("../results/tetris_results/tetris_eval_results.json", "w") as f:
-    #             json.dump(results, f, indent=2)
+    for model in results.keys():
+        for i in range(5):  # 5 trails
+            save_path = f"results/tetris_results/{model}_{i}_tetris_game_history.json"
+            results = run_tetris(model, save_path, rng=random.Random(42 + i))
+    
+            results[model]["final_score"].append(results["final_score"])
+            results[model]["max_stack_height"].append(results["max_stack_height"])
+            results[model]["holes"].append(results["holes"])
+            results[model]["bumps"].append(results["bumps"])
+            results[model]["height_delta_per_move"].append(results["height_delta_per_move"])
+            with open("../results/tetris_results/tetris_eval_results.json", "w") as f:
+                json.dump(results, f, indent=2)
