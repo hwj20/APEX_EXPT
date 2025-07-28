e6347866e67284e1578bb85dabba512ffdc117a1
hwj20 <912299489@qq.com>
2025-07-27 22:00:58 -0700

    update ques gen and commit llmrouter

--- experiments/physical_question_expt/dataset/generate_3_body_problem.py
+++ experiments/physical_question_expt/dataset/generate_3_body_problem.py
@@ -12,7 +12,7 @@ def generate_prompt():
     q = (
         "\n        Three bodies with masses {m1} kg, {m2} kg, and {m3} kg "
         "are located at positions ({x1}, {y1}) m, ({x2}, {y2}) m, and ({x3}, {y3}) m respectively. "
-        "Using G = 6.674e-11 N·m²/kg², calculate the acceleration components (ax, ay) of each body at t = 0.\n    ".format(
+        "Using G = 6.674e-11 N·m²/kg², calculate the acceleration components (ax, ay) of each body at t = 1s.\n    ".format(
             m1=masses[0], m2=masses[1], m3=masses[2],
             x1=positions[0][0], y1=positions[0][1],
             x2=positions[1][0], y2=positions[1][1],
